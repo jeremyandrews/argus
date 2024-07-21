abb1ecfc9de86624010a8557578dfc116213e729
Jeremy Andrews <jeremy@tag1consulting.com>
2024-07-21 13:39:40 -0700

    filter stdout noise

--- src/logging.rs
+++ src/logging.rs
@@ -1,15 +1,29 @@
 use std::io;
+use tracing::Level;
 use tracing_appender::rolling;
+use tracing_subscriber::filter::FilterFn;
 use tracing_subscriber::fmt;
+use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
 
 pub fn configure_logging() {
+    // Custom filter to ignore specific warnings
+    let custom_filter = FilterFn::new(|metadata| {
+        // Exclude specific warnings based on their target and message
+        if metadata.level() == &Level::WARN && metadata.target() == "html5ever::serialize" {
+            false
+        } else {
+            true
+        }
+    });
+
     let stdout_log = fmt::layer()
         .with_writer(io::stdout)
         .with_filter(EnvFilter::new(
             "info,llm_request=warn,web_request=warn,db=warn",
-        ));
+        ))
+        .with_filter(custom_filter);
 
     let file_appender = rolling::daily("logs", "app.log");
     let file_log = fmt::layer()
